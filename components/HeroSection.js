0d097178ea14df77395bfbb0947563edbdb2615e
Jyoti Bharti <76005177+stackpointer1@users.noreply.github.com>
2021-05-09 13:51:19 +0530

    init

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -7,7 +7,7 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='/videos/video-2.mp4'
+            <video src='/videos/video-1.mp4'
             autoPlay loop muted />
             <h1> ADVENTURE AWAITS </h1>
             <p>what are you waiting for?</p>
